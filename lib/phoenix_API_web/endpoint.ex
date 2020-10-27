f19580325bfbe2e082339382c65e81bfc25ce963
KushanChamindu <ra.kushan1997@gmail.com>
2020-10-27 22:18:21 +0530

    minor changes

--- lib/phoenix_API_web/endpoint.ex
+++ lib/phoenix_API_web/endpoint.ex
@@ -17,7 +17,7 @@ defmodule Phoenix_APIWeb.Endpoint do
   socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
 
   # Serve at "/" the static files from "priv/static" directory.
-  #
+  # 
   # You should set gzip to true if you are running phx.digest
   # when deploying your static files in production.
   plug Plug.Static,
